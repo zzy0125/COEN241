38dd5339b8d7e856df67098ce29232fee856bb8b
zzy0125 <116046813+zzy0125@users.noreply.github.com>
2022-11-02 16:40:51 -0700

    Add files via upload

---
+++ HW2/slack-interactive/handler.py
@@ -0,0 +1,29 @@
+import json
+import urllib
+
+def handle(req):
+    urlstring = urllib.unquote(req).decode('utf8').strip('payload=')
+    response = json.loads(urlstring)
+    data = {
+        "attachments": [
+            {
+                "replace_original": True,
+                "response_type": "ephemeral",
+                "fallback": "Required plain-text summary of the attachment.",
+                "color": "#36a64f",
+                "pretext": "Ahh yeah! Great choice, COEN 241 is absolutely amazing!",
+                "author_name": "",
+                "author_link": <INSERT YOUR GITHUB REPO URL>,
+                "author_icon": <INSERT YOUR GITHUB REPO PROFILE IMG URL>,
+                "title": "COEN 241",
+                "title_link": "https://www.scu.edu/engineering/academic-programs/department-of-computer-engineering/graduate/course-descriptions/",
+                "text": "Head over to COEN 241",
+                "image_url": "https://www.scu.edu/media/offices/umc/scu-brand-guidelines/visual-identity-amp-photography/visual-identity-toolkit/logos-amp-seals/Mission-Dont3.png",
+                "thumb_url": "https://www.scu.edu/engineering/academic-programs/department-of-computer-engineering/graduate/course-descriptions/",
+                "footer": "Slack Apps built on OpenFaas",
+                "footer_icon": "https://a.slack-edge.com/45901/marketing/img/_rebrand/meta/slack_hash_256.png",
+                "ts": 123456789
+            }
+        ]
+    }
+    return json.dumps(data)
